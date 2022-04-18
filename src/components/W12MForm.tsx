a2b35816293210bf82c7dd6c10e3eea5ad9e8755
Cenk <muzzyathome@gmail.com>
2022-04-18 13:43:50 +0100

    SpeciesName component

--- src/components/W12MForm.tsx
+++ src/components/W12MForm.tsx
@@ -1,11 +1,15 @@
 import { useState } from 'react';
+import SpeciesName from './SpeciesName';
 import W12MHeader from './W12MHeader';
 
 const W12MForm = () => {
+	
+	const [speciesName,setSpeciesName]= useState<string>('humans');
+
 	return (
 		<section className='w12MForm'>
 			<W12MHeader />
-			{/* REST OF FORM GOES HERE */}
+			{<SpeciesName speciesName={speciesName} onChangeSpeciesName={(e:any)=> setSpeciesName(e.target.value)}/>}
 		</section>
 	);
 };
