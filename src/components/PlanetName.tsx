00e7f894e8dfe38c44a15e3e20ca1c9a6eadd369
Cenk <muzzyathome@gmail.com>
2022-04-19 15:30:53 +0100

    A bit of reformating

--- src/components/PlanetName.tsx
+++ src/components/PlanetName.tsx
@@ -4,10 +4,10 @@ interface PlanetNameProps {
 }
 
 const PlanetName : React.FC<PlanetNameProps> = ({planetName,onChangePlanetName}) => (
-<>
+<p>
     <label htmlFor='planetName' >Planet Name: </label>
     <input id='planetName' type='text' value={planetName} onChange={onChangePlanetName}/>   
-</>
+</p>
 );
 
 export default PlanetName;
--- src/components/SpeciesName.tsx
+++ src/components/SpeciesName.tsx
@@ -4,10 +4,10 @@ interface SpeciesNameProps {
 }
 
 const SpeciesName : React.FC<SpeciesNameProps> = ({speciesName,onChangeSpeciesName}) => (
-<>
+<p>
     <label htmlFor='speciesName'>Species Name: </label>
     <input id='speciesName' type='text' value={speciesName} onChange={onChangeSpeciesName}/>
-</>
+</p>
 );
 
 export default SpeciesName;
