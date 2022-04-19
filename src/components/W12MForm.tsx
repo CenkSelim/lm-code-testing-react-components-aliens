bf3fa7dfbd251c2bc59a1a4729fa690e0f4c04a9
Cenk <muzzyathome@gmail.com>
2022-04-19 17:22:06 +0100

    Added form and button to log values to console

--- src/components/W12MForm.tsx
+++ src/components/W12MForm.tsx
@@ -11,22 +11,32 @@ const W12MForm = () => {
 	const [speciesName,setSpeciesName]= useState<string>('humans');
 	const [planetName,setPlanetName]=useState<string>('Earth');
 	const [numberOfBeings,setNumberOfBeings]=useState<string>('');
-	const [whatIs2Plus2,setWhatis2Plus2]=useState<string>('');
+	const [whatIs2Plus2,setWhatis2Plus2]=useState<string>('Not 4');
 	const [reasonForSparing,setReasonForSparing]=useState<string>('');
+	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		console.log(`speciesName: ${speciesName}, planetName: ${planetName}, 
+		numberOfBeings: ${numberOfBeings}, whatIs2Plus2: ${whatIs2Plus2}, 
+		reasonForSparing: ${reasonForSparing}`);
+  	};
+	
 
 	return (
-		<section className='w12MForm'>
-			<W12MHeader />
-			{
-				<>
-					<SpeciesName speciesName={speciesName} onChangeSpeciesName={(e:any)=> setSpeciesName(e.target.value)}/>
-					<PlanetName planetName={planetName} onChangePlanetName={(e:any)=>setPlanetName(e.target.value)}/>
-					<NumberOfBeings numberOfBeings={numberOfBeings} onChangeNumberOfBeings={(e:any)=>setNumberOfBeings(e.target.value)}/>
-					<WhatIs2Plus2 whatIs2Plus2={whatIs2Plus2} onChangeWhatis2Plus2={(e:any)=>setWhatis2Plus2(e.target.value)}/>
-					<ReasonForSparing reasonForSparing={reasonForSparing} onChangeReasonForSparing={(e:any)=>setReasonForSparing(e.target.value)}/>
-				</>
-			}
-		</section>
+		<form onSubmit={handleSubmit}>
+			<section className='w12MForm'>
+				<W12MHeader />
+				{
+					<>
+						<SpeciesName speciesName={speciesName} onChangeSpeciesName={(e:any)=> setSpeciesName(e.target.value)}/>
+						<PlanetName planetName={planetName} onChangePlanetName={(e:any)=>setPlanetName(e.target.value)}/>
+						<NumberOfBeings numberOfBeings={numberOfBeings} onChangeNumberOfBeings={(e:any)=>setNumberOfBeings(e.target.value)}/>
+						<WhatIs2Plus2 whatIs2Plus2={whatIs2Plus2} onChangeWhatis2Plus2={(e:any)=>setWhatis2Plus2(e.target.value)}/>
+						<ReasonForSparing reasonForSparing={reasonForSparing} onChangeReasonForSparing={(e:any)=>setReasonForSparing(e.target.value)}/>
+					</>
+				}
+			</section>
+			<button type = 'submit'>Form submit</button>
+		</form>
 	);
 };
 
