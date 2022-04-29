a8a8b5926175576bcd60bd539f07971bc3b84033
Cenk <muzzyathome@gmail.com>
2022-04-29 19:47:18 +0100

    Button test added

--- src/components/W12MForm.test.tsx
+++ src/components/W12MForm.test.tsx
@@ -1,7 +1,8 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import W12MForm from './W12MForm';
 
-test('renders form element', () => {
+it('renders form element', () => {
 	// we can hold onto the object returned from render()
 	// this object has a container property that we can destructure and inspect
 	const { container } = render(<W12MForm />);
@@ -9,4 +10,26 @@ test('renders form element', () => {
 	// the container is just a normal DOM element, so we can look at normal properties like '.firstChild'
 	// for example, the firstChild of our container should be our form element
 	expect(container.firstChild).toHaveClass('w12MForm');
+});
+
+it(`Given a form is rendered,
+	When the 'Form submit' button is clicked,
+	Then the handleSubmit function is called`, () => {
+
+	const logSpy = jest.spyOn(console, 'log');
+
+	render(<W12MForm />);
+  
+	const submitFormButton = screen
+		.getAllByRole('button')
+		.find((button) => button.textContent === 'Form submit');
+
+	expect(submitFormButton).toBeInTheDocument();
+
+	if (submitFormButton) {
+		userEvent.click(submitFormButton);
+	}
+
+	expect(logSpy).toHaveBeenCalledTimes(1);
+	expect(logSpy).toContain('Earth');
 });
