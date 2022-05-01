5a70cfb424ecc754a90520b53b435e2feccd2205
Cenk <muzzyathome@gmail.com>
2022-05-01 17:41:51 +0100

    Whatis2plus2 tests#

--- src/components/WhatIs2Plus2.test.tsx
+++ src/components/WhatIs2Plus2.test.tsx
@@ -1,6 +1,6 @@
 import {render, screen} from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import WhatIs2Plus2, {WhatIs2Plus2Props} from "./WhatIs2Plus2"
+import WhatIs2Plus2, {WhatIs2Plus2ErrorMessage, WhatIs2Plus2Props} from "./WhatIs2Plus2"
 
 describe('WhatIs2Plus2 component', () => {
 	it(`Given the required props,
@@ -11,6 +11,7 @@ describe('WhatIs2Plus2 component', () => {
             onChangeWhatis2Plus2 : () => {},
 		};
 		render(<WhatIs2Plus2 {...requiredProps} />);
+		userEvent.selectOptions(screen.getByLabelText('What is 2 + 2 :'), 'Not 4');
 		expect(screen.getByDisplayValue(requiredProps.whatIs2Plus2)).toBeInTheDocument();
 	});
 
@@ -31,5 +32,38 @@ describe('WhatIs2Plus2 component', () => {
         userEvent.selectOptions(screen.getByLabelText('What is 2 + 2 :'), '4');
 		expect(mockEntryHandler.mock.calls.length).toBe(1);
 		expect(mockEntryHandler.mock.results[0].value).toBe('4');
+	});
+});
+
+describe('SpeciesName component entry validattion', () => {	
+
+	it(`Given the required props,
+		When the component is rendered the entry with lesss than 3 characters
+		Then the appropriate error message should be present`, async () => {
+		const requiredProps: WhatIs2Plus2Props = {
+            whatIs2Plus2 : 'Not 4',
+            onChangeWhatis2Plus2 : () => {},
+		};
+		
+		render(<WhatIs2Plus2 {...requiredProps} />);
+
+        userEvent.selectOptions(screen.getByLabelText('What is 2 + 2 :'), 'Not 4');
+    	expect(await screen.findByText(WhatIs2Plus2ErrorMessage)).toBeInTheDocument();
+	});
+
+
+	it(`Given the required props,
+		When the component is rendered the entry with valid data  
+		Then no message should be present`, async () => {
+		
+		const requiredProps: WhatIs2Plus2Props = {
+            whatIs2Plus2 : 'Not 4',
+            onChangeWhatis2Plus2 : () => {},
+		};
+		
+		render(<WhatIs2Plus2 {...requiredProps} />);
+
+        userEvent.selectOptions(screen.getByLabelText('What is 2 + 2 :'), '4');
+    	expect(screen.queryByText(WhatIs2Plus2ErrorMessage)).not.toBeInTheDocument();
 	});
 });
--- src/components/WhatIs2Plus2.tsx
+++ src/components/WhatIs2Plus2.tsx
@@ -1,15 +1,40 @@
+import { useState } from "react";
+import ErrorMessage from "./ErrorMessage";
+
 export interface WhatIs2Plus2Props {
     whatIs2Plus2 : string;
     onChangeWhatis2Plus2 : (e:React.ChangeEvent<HTMLSelectElement>)=>void;
 }
 
-const WhatIs2Plus2:React.FC<WhatIs2Plus2Props> = ({whatIs2Plus2,onChangeWhatis2Plus2}) => (
-    <p>
-        <label htmlFor='whatIs2Plus2'>What is 2 + 2 : </label>
-        <select id='whatIs2Plus2' value={whatIs2Plus2} onChange={onChangeWhatis2Plus2}>
-            <option value="Not 4">Not 4</option>
-            <option value="4">4</option>
-        </select>
-    </p>
-);
+export const WhatIs2Plus2ErrorMessage = "You must be kidding!";
+
+const WhatIs2Plus2:React.FC<WhatIs2Plus2Props> = ({whatIs2Plus2,onChangeWhatis2Plus2}) => {
+   
+        const [ errorMessage, setErrorMessage ] = useState<string | undefined>('');
+
+		const validate : (value : string) => string | undefined = (value) => {
+        if(value.length < 1 || Number.isNaN(Number(value)) === true){
+            return WhatIs2Plus2ErrorMessage;
+        }
+        return undefined;
+    }
+  
+    return (
+        <>
+            <label htmlFor='whatIs2Plus2'>What is 2 + 2 : </label>
+            <select id='whatIs2Plus2' value={whatIs2Plus2} 
+                onChange={(e) => {
+					const errorMessage = validate(e.target.value);
+					setErrorMessage(errorMessage);
+					onChangeWhatis2Plus2(e);
+				    }
+                }>
+                <option value="Not 4">Not 4</option>
+                <option value="4">4</option>
+            </select>
+            <ErrorMessage errorMessage={errorMessage}/>
+        </>
+    );
+};
+
 export default WhatIs2Plus2;
